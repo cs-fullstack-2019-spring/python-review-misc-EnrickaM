c7ed39125f2c0986be891811eb4d047c9f5702cf
EnrickaM <marshallenricka@yahoo.com>
2019-01-24 09:25:03 -0600

    cw

---
+++ cw.py
@@ -0,0 +1,43 @@
+def main():
+    #problem1
+    problem2
+
+
+#Create a function that has a loop that quits with q.
+# If the user doesn't enter q, ask them to input another string.
+
+
+def problem1():
+    letter= input("Enter a letter.")
+    if letter=='q':
+        break
+    else:
+        print("TRY AGAIN")
+
+def exercise2_helper():
+
+# Write 2 functions: exercise2() and exercise2_helper(num1, num2, num3. operation)
+# The function exercise2_helper(num1, num2, num3) should expect 3 numbers, and an operation to perform as a String as parameters.
+
+def problem2():
+
+
+    def exercise2_helper():
+        number = str(int(input("")))
+        number2=str(int(input("")))
+        number3=str(int(input("")))
+        average= str(int("number" +"number2" +"number3", ))
+        print(average)
+
+
+
+
+
+
+
+
+
+
+
+    if __name__ == '__main__':
+        main()
